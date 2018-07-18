bb7b861012e37712ac227e92f8ce1b753432381a
aleserche <aleserche@gmail.com>
2018-07-18 08:12:22 +0500

    Removed the unused script 'changeKYCStatus.sh'.

--- scripts/changeKYCStatus.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-EMAIL=$1
-KYC_STATUS=${2:-'verified'}
-docker-compose exec mongo mongo localhost/ico-dashboard --eval "db.user.findOneAndUpdate({ email: '${EMAIL}' }, { \$set: { kycStatus: '${KYC_STATUS}' } })"
