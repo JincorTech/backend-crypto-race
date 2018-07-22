90035136f63471a8c1db04d89c600fc81d5224cf
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-07-22 18:59:09 +0700

    Tracks

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -148,6 +148,7 @@ tracks.on('connect', async socket => {
   socket.emit('init', {tracks: tracks});
   socket.broadcast.emit('init', {tracks: tracks});
   socket.on('joinTrack', async (joinData: any) =>  {
+    console.log("Track id: ", joinData.trackId);
     await gameClient.joinToTrack(user, user.mnemonic, joinData.trackId);
     socket.emit('init',{  } /*strafeData*/);
     socket.broadcast.emit('init',{} /*strafeData*/);
