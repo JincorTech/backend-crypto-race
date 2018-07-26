68c3829b034cd4920e328db51ed1ce881c4972b5
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-07-26 18:52:33 +0700

    New Socket

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -10,7 +10,7 @@ import { createConnection, ConnectionOptions, getConnection } from 'typeorm';
 import { AuthClientType } from '../services/auth.client';
 import { TrackServiceType, TrackServiceInterface } from '../services/track.service';
 import { User } from '../entities/user';
-import { Track } from '../entities/track';
+import {Track, TRACK_STATUS_ACTIVE} from '../entities/track';
 import { ancestorWhere } from 'tslint';
 // import { jwt_decode } from 'jwt-decode';
 
@@ -41,95 +41,81 @@ createConnection(ormOptions).then(async connection => {
     httpsServer.listen(config.app.httpsPort);
   }
 }).catch(error => console.log('TypeORM connection error: ', error));
-const sock = io.of('/');
-const chat = io.of('/chat');
-const race = io.of('/race');
-const tracks = io.of('/tracks');
 
-const messages = [];
+
+const messages = {};
 const authClient: AuthClientInterface = container.get(AuthClientType);
 const trackService: TrackServiceInterface = container.get(TrackServiceType);
 
-chat.use(async(socket, next) => {
+const sock = io.of('/');
+
+sock.use(async(socket, next) => {
   let handshake = socket.handshake;
   const result = await authClient.verifyUserToken(handshake.query.token);
   socket.handshake.query.email = result.login;
   next();
 });
 
-race.use(async(socket, next) => {
-  let handshake = socket.handshake;
-  const result = await authClient.verifyUserToken(handshake.query.token);
-  socket.handshake.query.email = result.login;
-  next();
-});
 
-tracks.use(async(socket, next) => {
-  let handshake = socket.handshake;
-  const result = await authClient.verifyUserToken(handshake.query.token);
-  socket.handshake.query.email = result.login;
-  next();
-});
-
-chat.on('connect', async socket => {
+sock.on('connect', async socket => {
   const user = await getConnection().mongoManager.findOne(User, {where: {email: socket.handshake.query.email}});
-  socket.on('requestInitData', data => {
-    socket.emit('responseInitData', messages);
+
+
+  /**
+   * ================== TRACK SECTION ===============
+   */
+  socket.on('getTracks', async () => {
+
+    let tracks = await getConnection().mongoManager.find(Track, {take: 1000});
+    if (tracks.filter((track) => { return track.status === 'awaiting'; }).length < 2) {
+      tracks.push(await trackService.internalCreateTrack('1'));
+      tracks.push(await trackService.internalCreateTrack('0'));
+    }
+    socket.emit('initTracks', {tracks: tracks});
+    socket.broadcast.emit('initTracks', {tracks: tracks});
+  });
+
+  socket.on('joinTrack', async (joinData: any) => {
+    const track = await trackService.joinToTrack(user, user.mnemonic, joinData.trackId);
+    if (!track) {
+      socket.to(socket.id).emit('error', {message: "Track not found"});
+      return;
+    }
+
+    socket.join('tracks_' + joinData.trackId, () => {
+      socket.in('tracks_' + joinData.trackId).emit('joinedTrack', joinData);
+      if (track.status === TRACK_STATUS_ACTIVE) {
+        let init: InitRace = { raceName: track.id.toHexString(), start: Date.now(), end: Date.now() + 300, players: track.players};
+        socket.in('tracks_' + joinData.trackId).emit('start', init);
+      }
+    });
+
+    tracks = await getConnection().mongoManager.find(Track, {take: 1000});
+    socket.emit('initTracks',{tracks: tracks});
+    socket.broadcast.emit('initTracks',{tracks: tracks});
+  });
+
+  socket.on('moveX', (strafeData: Strafe) => {
+    socket.in('tracks_' + track.id.toString()).emit('moveXupdate', strafeData);
+  });
+
+
+  /**
+   * ================== CHAT SECTION ===============
+   */
+  socket.on('joinChat', async (joinData: any) => {
+    socket.join('chats_' + joinData.trackId, () => {
+      if(messages[joinData.trackId].length === 0) {
+        messages[joinData.trackId] = [];
+      }
+      socket.in('chats_' + joinData.trackId).emit('joinedChat', messages[joinData.trackId]);
+    });
   });
 
   socket.on('message', message => {
-    messages.push({ author: user.name, userId: user.id, ts: Date.now(), message });
-    socket.emit('update', messages);
-    socket.broadcast.emit('update', messages);
-  });
-});
-
-race.on('connect', async socket => {
-  const user = await getConnection().mongoManager.findOne(User, {where: {email: socket.handshake.query.email}});
-  const track = await getConnection().mongoManager.findOne(Track, {
-    where: {
-      users: {
-        '$in': [user.id.toString()]
-      }
-    }
-  });
-  if (!track) {
-    socket.disconnect();
-  }
-  socket.join(track.id.toString());
-  let init: InitRace = { raceName: track.id.toHexString(), start: Date.now(), end: Date.now() + 300, players: track.players};
-
-  socket.to(track.id.toString()).emit('init', init);
-  socket.on('moveX', (strafeData: Strafe) => {
-    socket.to(track.id.toString()).emit('moveXupdate', strafeData);
-    socket.to(track.id.toString()).broadcast.emit('moveXupdate', strafeData);
+    messages[message.chatId].push({ author: user.name, userId: user.id, ts: Date.now(), message });
+    socket.in('chats_' + message.chatId).emit('updateChat', messages[message.chatId]);
   });
 
 });
 
-tracks.on('connect', async socket => {
-  const user = await getConnection().mongoManager.findOne(User, {where: {email: socket.handshake.query.email }});
-  let tracks = await getConnection().mongoManager.find(Track, {take: 1000});
-  if (tracks.filter((track) => { return track.status === 'awaiting'; }).length < 2) {
-    tracks.push(await trackService.internalCreateTrack('100'));
-    tracks.push(await trackService.internalCreateTrack('0'));
-  }
-  socket.emit('init', {tracks: tracks});
-  socket.broadcast.emit('init', {tracks: tracks});
-
-  socket.on('joinTrack', async(joinData: any) => {
-    const track = await trackService.joinToTrack(user, user.mnemonic, joinData.trackId);
-    if (track) {
-      socket.join('tracks' + joinData.trackId, function() {
-        socket.to(socket.id).emit('joined', joinData);
-        if (track.numPlayers === track.maxPlayers) {
-          socket.in('tracks' + joinData.trackId).emit('start', { trackId: joinData.trackId });
-        }
-      });
-    }
-    tracks = await getConnection().mongoManager.find(Track, {take: 1000});
-    socket.emit('init',{tracks: tracks});
-    socket.broadcast.emit('init',{tracks: tracks});
-  });
-
-});
