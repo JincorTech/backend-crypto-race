cefd271d6c2684a051bb0d4068fe08f73bfa63c1
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-07-27 01:17:43 +0700

    Add player to track fixed

--- src/services/track.service.ts
+++ src/services/track.service.ts
@@ -3,7 +3,7 @@ import { User } from '../entities/user';
 import { Portfolio } from '../entities/portfolio';
 import { Track, TRACK_TYPE_BACKEND, TRACK_STATUS_AWAITING, TRACK_TYPE_USER, TRACK_STATUS_ACTIVE } from '../entities/track';
 import { Web3ClientInterface, Web3ClientType } from './web3.client';
-import {getConnection, MongoRepository, getConnection} from 'typeorm';
+import {getConnection, MongoRepository} from 'typeorm';
 import { ObjectID } from 'mongodb';
 
 export interface TrackServiceInterface {
