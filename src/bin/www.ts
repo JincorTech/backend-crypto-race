a1ef41568c92a391f291304f600181647814036a
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-07-27 20:22:09 +0700

    Join room on join track

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -108,9 +108,10 @@ createConnection(ormOptions).then(async connection => {
         io.sockets.in(socket.id).emit('error', {message: "You can not join inactive track"});
         return;
       }
-      let init: InitRace = { id: track.id.toString(), raceName: track.id.toHexString(), start: Date.now(), end: Date.now() + 300, players: track.players};
-      io.sockets.in(socket.id).emit('start', init);
-
+      socket.join('tracks_' + joinData.trackId, () => {
+        let init: InitRace = { id: track.id.toString(), raceName: track.id.toHexString(), start: Date.now(), end: Date.now() + 300, players: track.players};
+        io.sockets.in(socket.id).emit('start', init);
+      });
     });
 
     socket.on('moveX', (strafeData: Strafe) => {
