00d8e3032850a72afad99ae2637536f6dc2a98ea
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-07-28 02:16:44 +0700

    Appropriate track start

--- src/handlers/crypto.currency.handler.ts
+++ src/handlers/crypto.currency.handler.ts
@@ -7,39 +7,37 @@ export interface CryptoCurrencyHandlerInterface { }
 
 export class CryptoCurrencyHandler implements CryptoCurrencyHandlerInterface {
   constructor() {
-    const now = Date.now();
-    let currentTime = now + (5 - (now % 5));
     cryptoSocket.start('bittrex',['LTCUSD','BTCUSD', 'XRPUSD', 'ETHUSD', 'BCHUSD']);
     setInterval(
       async function() {
+        const now = Date.now();
+        let currentTime = now + (5 - (now % 5));
         let rate = cryptoSocket.Exchanges['bittrex'];
-        await getConnection().mongoManager.save(Currency, Currency.createCurrency({
+        getConnection().mongoManager.save(Currency, Currency.createCurrency({
           timestamp: currentTime,
           name: 'LTC',
           usd: rate.LTCUSD
         }));
-        await getConnection().mongoManager.save(Currency, Currency.createCurrency({
+        getConnection().mongoManager.save(Currency, Currency.createCurrency({
           timestamp: currentTime,
           name: 'ETH',
           usd: rate.ETHUSD
         }));
-        await getConnection().mongoManager.save(Currency, Currency.createCurrency({
+        getConnection().mongoManager.save(Currency, Currency.createCurrency({
           timestamp: currentTime,
           name: 'BTC',
           usd: rate.BTCUSD
         }));
-        await getConnection().mongoManager.save(Currency, Currency.createCurrency({
+        getConnection().mongoManager.save(Currency, Currency.createCurrency({
           timestamp: currentTime,
           name: 'XRP',
           usd: rate.XRPUSD
         }));
-        await getConnection().mongoManager.save(Currency, Currency.createCurrency({
+        getConnection().mongoManager.save(Currency, Currency.createCurrency({
           timestamp: currentTime,
           name: 'BCH',
           usd: rate.BCHUSD
         }));
-
-        currentTime += 5;
       },
       5000
     );
