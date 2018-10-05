3aea87a6263b3f3dbf3535df25e72a3e3601a4ac
aleserche <aleserche@gmail.com>
2018-10-05 11:53:48 +0500

    Fix.

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -72,9 +72,9 @@ createConnection(ormOptions).then(async connection => {
   const botEmails = ['bot1@secrettech.io', 'bot2@secrettech.io', 'bot3@secrettech.io', 'bot4@secrettech.io', 'bot5@secrettech.io'];
   await createBots(botEmails);
 
-  const sock = io.of('/');
+  const rootSocket = io.of('/');
 
-  sock.use(async(socket, next) => {
+  rootSocket.use(async(socket, next) => {
     let handshake = socket.handshake;
     if (handshake.query.token) {
       const result = await authClient.verifyUserToken(handshake.query.token);
@@ -85,23 +85,23 @@ createConnection(ormOptions).then(async connection => {
     }
   });
 
-  sock.on('connect', async socket => {
-    if (!socket.handshake.query.email) {
-      io.sockets.in(socket.id).emit('error', {message: 'User not found'});
-      socket.disconnect(true);
+  rootSocket.on('connect', async subSocket => {
+    if (!subSocket.handshake.query.email) {
+      io.sockets.in(subSocket.id).emit('error', {message: 'User not found'});
+      subSocket.disconnect(true);
       return false;
     }
 
-    const user = await getConnection().mongoManager.findOne(User, {where: {email: socket.handshake.query.email}});
+    const user = await getConnection().mongoManager.findOne(User, {where: {email: subSocket.handshake.query.email}});
 
     if (!user) {
-      io.sockets.in(socket.id).emit('error', {message: 'User not found'});
-      socket.disconnect(true);
+      io.sockets.in(subSocket.id).emit('error', {message: 'User not found'});
+      subSocket.disconnect(true);
       return false;
     }
 
-    socket.on('reqProfile', async() => {
-      io.sockets.in(socket.id).emit('resProfile', {
+    subSocket.on('reqProfile', async() => {
+      subSocket.emit('resProfile', {
         picture: user.picture,
         balance: await web3Client.getEthBalance(user.ethWallet.address),
         address: user.ethWallet.address,
@@ -112,36 +112,36 @@ createConnection(ormOptions).then(async connection => {
     /**
      * ================== TRACK SECTION ===============
      */
-    socket.on('getTracks', async() => {
+    subSocket.on('getTracks', async() => {
       let tracks = await getConnection().mongoManager.find(Track, {take: 1000});
       for (let i = 2; i <= config.game.numTrack; i++) {
         if (tracks.filter((track) => { return track.status === 'awaiting' && track.maxPlayers === i; }).length === 0) {
           tracks.push({ ...(await trackService.internalCreateTrack(config.game.betAmount, i)), duration: 300000 } as Track);
         }
       }
-      socket.emit('initTracks', {tracks: tracks});
-      socket.broadcast.emit('initTracks', {tracks: tracks});
+      subSocket.emit('initTracks', {tracks: tracks});
+      subSocket.broadcast.emit('initTracks', {tracks: tracks});
     });
 
-    socket.on('joinTrack', async(joinData: any) => {
+    subSocket.on('joinTrack', async(joinData: any) => {
       let track = await trackService.getTrackById(joinData.trackId);
       if (!track) {
-        io.sockets.in(socket.id).emit('error', {message: 'Track not found'});
+        io.sockets.in(subSocket.id).emit('error', {message: 'Track not found'});
         return;
       }
       if (track.isActive) {
-        io.sockets.in(socket.id).emit('error', {message: 'Track is already active'});
+        io.sockets.in(subSocket.id).emit('error', {message: 'Track is already active'});
         return;
       }
 
       if ((joinData.fuel as Array<number>).reduce((p, c) => p + c) !== 100) {
-        io.sockets.in(socket.id).emit('error', {message: 'Fuel total is not equal 100%'});
+        io.sockets.in(subSocket.id).emit('error', {message: 'Fuel total is not equal 100%'});
         return;
       }
 
       track = await trackService.joinToTrack(user, user.mnemonic, joinData.trackId, joinData.fuel, joinData.ship);
       if (!track) {
-        io.sockets.in(socket.id).emit('error', {message: 'Can not join track'});
+        io.sockets.in(subSocket.id).emit('error', {message: 'Can not join track'});
       }
 
       trackQueue.addJobWaitNewUsers({
@@ -149,7 +149,7 @@ createConnection(ormOptions).then(async connection => {
         numPlayers: track.numPlayers
       });
 
-      socket.join('tracks_' + joinData.trackId, async() => {
+      subSocket.join('tracks_' + joinData.trackId, async() => {
         io.sockets.in('tracks_' + joinData.trackId).emit('joinedTrack', joinData);
         if (track.status === TRACK_STATUS_ACTIVE) {
           let init: InitRace = { id: track.id.toString(), raceName: track.id.toHexString(), start: track.start * 1000, end: track.end * 1000, players: track.players};
@@ -169,38 +169,38 @@ createConnection(ormOptions).then(async connection => {
       });
 
       const tracks = await getConnection().mongoManager.find(Track, {take: 1000});
-      socket.emit('initTracks', {tracks: tracks});
-      socket.broadcast.emit('initTracks', {tracks: tracks});
+      subSocket.emit('initTracks', {tracks: tracks});
+      subSocket.broadcast.emit('initTracks', {tracks: tracks});
     });
 
-    socket.on('loadTrack', async(joinData: any) => {
+    subSocket.on('loadTrack', async(joinData: any) => {
 
       const track = await trackService.getTrackById(joinData.trackId);
       if (!track) {
-        io.sockets.in(socket.id).emit('error', {message: 'Track not found'});
+        io.sockets.in(subSocket.id).emit('error', {message: 'Track not found'});
         return;
       }
       if (track.status !== TRACK_STATUS_ACTIVE) {
-        io.sockets.in(socket.id).emit('error', {message: 'You can not join inactive track'});
+        io.sockets.in(subSocket.id).emit('error', {message: 'You can not join inactive track'});
         return;
       }
-      socket.join('tracks_' + joinData.trackId, () => {
+      subSocket.join('tracks_' + joinData.trackId, () => {
         let init: InitRace = { id: track.id.toString(), raceName: track.id.toHexString(), start: track.start * 1000, end: track.end * 1000, players: track.players};
-        io.sockets.in(socket.id).emit('start', init);
+        io.sockets.in(subSocket.id).emit('start', init);
       });
 
     });
 
-    socket.on('moveX', (strafeData: Strafe) => {
+    subSocket.on('moveX', (strafeData: Strafe) => {
       io.sockets.in('tracks_' + strafeData.trackId).emit('moveXupdate', strafeData);
     });
 
     /**
      * ================== CHAT SECTION ===============
      */
-    socket.on('joinChat', async(joinData: any) => {
+    subSocket.on('joinChat', async(joinData: any) => {
 
-      socket.join('chats_' + joinData.trackId, () => {
+      subSocket.join('chats_' + joinData.trackId, () => {
         if (!messages[joinData.trackId] || messages[joinData.trackId].length === 0) {
           messages[joinData.trackId] = [];
         }
@@ -209,7 +209,7 @@ createConnection(ormOptions).then(async connection => {
 
     });
 
-    socket.on('message', message => {
+    subSocket.on('message', message => {
 
       messages[message.chatId].push({ author: user.name, userId: user.id, ts: Date.now(), message });
       io.sockets.in('chats_' + message.chatId).emit('updateChat', messages[message.chatId]);
